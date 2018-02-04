65912915367b8f9d143b3a7ec2d86f47faf68f26
Francesca Bellini <fbellini@cern.ch>
2018-02-04 14:44:19 +0100

    Changed to save lists in one single output file

--- phiXeXe/projectPhiXeXe.C
+++ phiXeXe/projectPhiXeXe.C
@@ -21,49 +21,10 @@ void projectPhiXeXe( const char *nameData = "20180123_RsnOut.root",
   //generalia
   const Int_t kNhistosData = 4;
   outName.Append(binning.Data());
-  
-  //output
-  gSystem->Exec(Form("mkdir %s", outName.Data()));
-  TFile *dummy = new TFile(Form("%s/proj_%s", outName.Data(), nameData),"RECREATE");
-  TList *out = new TList();
-  dummy->cd();
-  ptbins->Write("ptbins");
-  centbins->Write("centbins");
 
-  //output lists - define once and then clear them
-  TList lUnlikePM;
-  lUnlikePM.SetName(Form("%s_Unlike", (!isMC ? "Data" : "MC")));
-  TList lLikePP;
-  lLikePP.SetName(Form("%s_LikePP", (!isMC ? "Data" : "MC")));
-  TList lLikeMM;
-  lLikeMM.SetName(Form("%s_LikeMM", (!isMC ? "Data" : "MC")));
-  TList lMixingPM;
-  lMixingPM.SetName(Form("%s_Mixing", (!isMC ? "Data" : "MC")));
-  
-  // open input file
-  TFile *fileData;
-  TList *listData;
-  fileData = TFile::Open(nameData);
-  if (fileData && fileData->IsOpen()) listData = (TList*)fileData->Get(listName);
-  
-  //get input histograms
-  TH3F* hInput[kNhistosData]= {0,0,0,0};
-  if (listData) {
-    hInput[ 0] = (TH3F*)listData->FindObject("PhiXeXeData_Unlike_tpc3s");
-    hInput[ 1] = (TH3F*)listData->FindObject("PhiXeXeData_Mixing_tpc3s");
-    hInput[ 2] = (TH3F*)listData->FindObject("PhiXeXeData_LikePP_tpc3s");
-    hInput[ 3] = (TH3F*)listData->FindObject("PhiXeXeData_LikeMM_tpc3s");
-    
-    // rename
-    hInput[ 0]->SetName("Data_UnlikePM");
-    hInput[ 1]->SetName("Data_MixingPM");
-    hInput[ 2]->SetName("Data_LikePP");
-    hInput[ 3]->SetName("Data_LikeMM");
-  }
-
-
-  
-  // define binning
+  //------------------------------
+  //define binning
+  //------------------------------
   Double_t centA[] = {0.0, 30.0, 60.0, 90.0};   
   Double_t centB[]  = {0.0, 20.0, 40.0, 60.0, 80.0};
   Double_t   pt1[] = {0.5, 1.00, 1.50, 2.00, 2.50, 3.00, 3.5, 4.00, 4.5, 5.0, 6.0, 7.0, 10.00};
@@ -88,22 +49,69 @@ void projectPhiXeXe( const char *nameData = "20180123_RsnOut.root",
 
   if (binning.Contains("A")) {
     ncent = sizeof(centA) / sizeof(centA[0]) - 1;
-    ptbins = new TAxis(ncent, centA);
+    centbins = new TAxis(ncent, centA);
   }  else
     if (binning.Contains("B")) {
       ncent = sizeof(centB) / sizeof(centB[0]) - 1;
-      ptbins = new TAxis(ncent, centB);
+      centbins = new TAxis(ncent, centB);
     }
+
+  //------------------------------
+  // open input file
+  //------------------------------
+  TFile *fileData = TFile::Open(nameData);
+  if (!fileData || !fileData->IsOpen()) { Printf("Invalid file passed as input. Doing nothing."); return;}
   
-  // split histograms
+  TList *listData = listData = (TList*)fileData->Get(listName);
+  if (!listData) { Printf("Invalid list passed as input. Doing nothing."); return;}
+    
+  //get input histograms
+  TH3F* hInput[kNhistosData] = {0,0,0,0}; 
+  hInput[0] = (TH3F*)listData->FindObject("PhiXeXeData_Unlike_tpc3s");
+  hInput[1] = (TH3F*)listData->FindObject("PhiXeXeData_LikePP_tpc3s");
+  hInput[2] = (TH3F*)listData->FindObject("PhiXeXeData_LikeMM_tpc3s");
+  hInput[3] = (TH3F*)listData->FindObject("PhiXeXeData_Mixing_tpc3s");
+
+  if (!hInput[0] || !hInput[1] || !hInput[2] || !hInput[3]) { Printf("Invalid histogram requested as input. Doing nothing."); return;}
+
+  // rename
+  hInput[0]->SetName("hUnlikePM");
+  hInput[1]->SetName("hLikePP");
+  hInput[2]->SetName("hLikeMM");
+  hInput[3]->SetName("hMixingPM");
+
+  
+  //------------------------------
+  //output
+  //------------------------------
+  gSystem->Exec(Form("mkdir %s", outName.Data()));
+  TFile *fileOut = new TFile(Form("%s/proj_%s", outName.Data(), nameData),"RECREATE");
+  //TList *out = new TList();
+  fileOut->cd();
+  ptbins->Write("ptbins");
+  centbins->Write("centbins");
+
+  //output lists - define once and then clear them
+  TList * lUnlikePM = new TList();  lUnlikePM->SetName("UnlikePM");
+  TList * lLikePP = new TList();    lLikePP->SetName("LikePP");
+  TList * lLikeMM = new TList();    lLikeMM->SetName("LikeMM");
+  TList * lMixingPM  = new TList();  lMixingPM->SetName("MixingPM");
+
+  //------------------------------
+  // project histograms
+  //------------------------------
   projectorTH3_InvMass_Centrality_Pt projector;
-   
+  TList * out = lUnlikePM; // support list
+  
   // loop on inputs
   for (Int_t i = 0; i < kNhistosData; i++) {
     if (!hInput[i]) continue;
-    // projector.SetPrefix(hInput[i]->GetName());
-    // projector.MultiProj(npt, pt, hInput[i], out);
     projector.SetPrefix(hInput[i]->GetName());
+    
+    if (i == 1) out = lLikePP;
+    if (i == 2) out = lLikeMM;
+    if (i == 3) out = lMixingPM;
+    
     if (binning.Contains("A1")) projector.MultiProjPtCent(npt, pt1, ncent, centA,  hInput[i], out);
     else if (binning.Contains("B1")) projector.MultiProjPtCent(npt, pt1, ncent, centB,  hInput[i], out);
     else if (binning.Contains("A2")) projector.MultiProjPtCent(npt, pt2, ncent, centA,  hInput[i], out);
@@ -111,99 +119,47 @@ void projectPhiXeXe( const char *nameData = "20180123_RsnOut.root",
     else if (binning.Contains("A3")) projector.MultiProjPtCent(npt, pt3, ncent, centA,  hInput[i], out);
     else if (binning.Contains("B3")) projector.MultiProjPtCent(npt, pt3, ncent, centB,  hInput[i], out);
   }
+  Printf(":::: Projected according to binning strategy %s", binning.Data());
 
-  //save separate files for each centrality bin
-  if (saveSeparatelyByCent) {
-    for (Int_t icentbin=0; icentbin<ncent;icentbin++){
-      lUnlikePM.Clear();
-      lMixingPM.Clear();
-      lLikePP.Clear();
-      lLikeMM.Clear();
-      FillListForCentrality(out,&lUnlikePM,icentbin);
-      FillListForCentrality(out,&lMixingPM,icentbin);
-      FillListForCentrality(out,&lLikePP,icentbin);
-      FillListForCentrality(out,&lLikeMM,icentbin);
-
-      //output file
-      TFile *fout = TFile::Open(Form("%s/proj_%i.root", outName.Data(), icentbin), "RECREATE");
-      // save into a file
-      fout->cd();   
-      ptbins->Write("ptbins");
-      centbins->Write("centbins");
-      //Printf("=========################ DEBUG %i",lUnlikePM.GetEntries());
-      //make-up
-      for (Int_t ih = 0; ih<lUnlikePM.GetEntries();ih++){
-	((TH1F*)lUnlikePM.At(ih))->SetLineColor(kBlack);
-	((TH1F*)lUnlikePM.At(ih))->SetMarkerColor(kBlack);
-	((TH1F*)lUnlikePM.At(ih))->SetMarkerStyle(20);
-	((TH1F*)lUnlikePM.At(ih))->SetMarkerSize(0.8); 
-	((TH1F*)lUnlikePM.At(ih))->SetLineWidth(2);
-	((TH1F*)lUnlikePM.At(ih))->SetFillColor(kGray);
-	((TH1F*)lUnlikePM.At(ih))->SetFillStyle(3002);
-	((TH1F*)lUnlikePM.At(ih))->SetDrawOption("b");
-      }
-      for (Int_t ih = 0; ih<lMixingPM.GetEntries();ih++){  
-	((TH1F*)lMixingPM.At(ih))->SetLineColor(kMagenta-2);
-	((TH1F*)lMixingPM.At(ih))->SetMarkerColor(kMagenta-2);
-	((TH1F*)lMixingPM.At(ih))->SetMarkerStyle(1);
-	((TH1F*)lMixingPM.At(ih))->SetLineWidth(2);
-      }
-      for (Int_t ih = 0; ih<lLikePP.GetEntries();ih++){
-	((TH1F*)lLikePP.At(ih))->SetLineColor(kRed);
-	((TH1F*)lLikePP.At(ih))->SetMarkerColor(kRed);
-	((TH1F*)lLikePP.At(ih))->SetMarkerStyle(1);
-	((TH1F*)lLikePP.At(ih))->SetLineWidth(2);
-      }
-      for (Int_t ih = 0; ih<lLikeMM.GetEntries();ih++){
-	((TH1F*)lLikeMM.At(ih))->SetLineColor(kBlue);
-	((TH1F*)lLikeMM.At(ih))->SetMarkerColor(kBlue);
-	((TH1F*)lLikeMM.At(ih))->SetMarkerStyle(1);
-	((TH1F*)lLikeMM.At(ih))->SetLineWidth(2);
-      }          
-      lUnlikePM.Write(hInput[0]->GetName(), TObject::kSingleKey);
-      lMixingPM.Write(hInput[1]->GetName(), TObject::kSingleKey);
-      lLikePP.Write(hInput[2]->GetName(), TObject::kSingleKey);
-      lLikeMM.Write(hInput[3]->GetName(), TObject::kSingleKey);
-      fout->Close();
-    }
+  for (Int_t ih = 0; ih<lUnlikePM->GetEntries();ih++){
+    ((TH1F*)lUnlikePM->At(ih))->SetLineColor(kBlack);
+    ((TH1F*)lUnlikePM->At(ih))->SetMarkerColor(kBlack);
+    ((TH1F*)lUnlikePM->At(ih))->SetMarkerStyle(20);
+    ((TH1F*)lUnlikePM->At(ih))->SetMarkerSize(0.8); 
+    ((TH1F*)lUnlikePM->At(ih))->SetLineWidth(2);
+    ((TH1F*)lUnlikePM->At(ih))->SetFillColor(kGray);
+    ((TH1F*)lUnlikePM->At(ih))->SetFillStyle(3002);
+    ((TH1F*)lUnlikePM->At(ih))->SetDrawOption("b");
   }
-  // write the full list on dummy file
-  dummy->cd();
-  out->Write();
-  dummy->Close();
+  for (Int_t ih = 0; ih<lMixingPM->GetEntries();ih++){  
+    ((TH1F*)lMixingPM->At(ih))->SetLineColor(kMagenta-2);
+    ((TH1F*)lMixingPM->At(ih))->SetMarkerColor(kMagenta-2);
+    ((TH1F*)lMixingPM->At(ih))->SetMarkerStyle(1);
+    ((TH1F*)lMixingPM->At(ih))->SetLineWidth(2);
+  }
+  for (Int_t ih = 0; ih<lLikePP->GetEntries();ih++){
+    ((TH1F*)lLikePP->At(ih))->SetLineColor(kRed);
+    ((TH1F*)lLikePP->At(ih))->SetMarkerColor(kRed);
+    ((TH1F*)lLikePP->At(ih))->SetMarkerStyle(1);
+    ((TH1F*)lLikePP->At(ih))->SetLineWidth(2);
+  }
+  for (Int_t ih = 0; ih<lLikeMM->GetEntries();ih++){
+    ((TH1F*)lLikeMM->At(ih))->SetLineColor(kBlue);
+    ((TH1F*)lLikeMM->At(ih))->SetMarkerColor(kBlue);
+    ((TH1F*)lLikeMM->At(ih))->SetMarkerStyle(1);
+    ((TH1F*)lLikeMM->At(ih))->SetLineWidth(2);
+  }          
+  fileOut->cd();
+  lUnlikePM->Write(hInput[0]->GetName(), TObject::kSingleKey);
+  lLikePP->Write(hInput[1]->GetName(), TObject::kSingleKey);
+  lLikeMM->Write(hInput[2]->GetName(), TObject::kSingleKey);
+  lMixingPM->Write(hInput[3]->GetName(), TObject::kSingleKey);
+  fileOut->Close();
+  
+  Printf(":::: Output written in %s", fileOut->GetName());
   return;
    
 }
-//---------------------------------------------
-void FillListForCentrality(TList * listIn, TList * listOut, Int_t icentBin)
-{
-  //selects all type of histos per centrality bin and saves them in separate lists  
-  TString listName = listOut->GetName();
-  TString type;
-  
-  if (listName.Contains("Unlike")) type = "Unlike";
-  if (listName.Contains("LikeMM")) type = "LikeMM";
-  if (listName.Contains("LikePP")) type = "LikePP";
-  if (listName.Contains("Mixing")) type = "Mixing";
-  
-  Int_t nhisto = listIn->GetEntries();
-  for (Int_t ih=0;ih<nhisto;ih++){
-    TH1F * dummy = (TH1F *)listIn->At(ih);
-    TString hName = dummy->GetName();
-    if (hName.Contains(type.Data())){
-      if (hName.Contains(Form("centBin%02d",icentBin))){
-	listOut->AddLast(dummy);
-	Printf("Histo found with name: %s",hName.Data());
-      }
-    }
-  } 
-  
-  Printf("============== Output");
-  listOut->ls();
-  return;
-}
-
-
 
 
 
