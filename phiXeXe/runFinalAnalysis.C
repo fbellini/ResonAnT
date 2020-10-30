48c99778a54fdc4c3c8a440a7c67bde42725f8a1
Francesca Bellini <fbellini@cern.ch>
2020-10-30 12:40:06 +0100

    modified date fo final spectra

--- phiXeXe/runFinalAnalysis.C
+++ phiXeXe/runFinalAnalysis.C
@@ -1,8 +1,12 @@
-#include "/Users/fbellini/alice/macros/ResonAnT/phiXeXe/projectPhiXeXe.C"
-
+//#include "/Users/fbellini/alice/macros/ResonAnT/phiXeXe/projectPhiXeXe.C"
+#include "/Users/fbellini/alice/macros/ResonAnT/phiXeXe/FitSpectrum.C"
 void runFinalAnalysis()
 {
-  projectPhiXeXe("20200615_RsnOut_B3.root", "phi", "RsnOut_tpc2sPtDep_tof3sveto5smism", "B3", 0, 0);
-  
+  //projectPhiXeXe("20200615_RsnOut_B3.root", "phi", "RsnOut_tpc2sPtDep_tof3sveto5smism", "B3", 0, 0);
+  FitSpectrum(-1, "bgbw", 0.5, 10.0, "30oct20");
+  FitSpectrum(-1, "boltz", 0.5, 10.0, "30oct20");
+  FitSpectrum(-1, "bose", 0.5, 10.0, "30oct20");
+  FitSpectrum(-1, "levy", 0.5, 10.0, "30oct20");
+  FitSpectrum(-1, "mtexp", 0.5, 10.0, "30oct20");
   return;
 }
