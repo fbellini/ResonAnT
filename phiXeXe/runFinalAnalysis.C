7b54656ff6305e35e4636efddb3680aa53b9ee26
Francesca Bellini <fbellini@cern.ch>
2020-12-07 16:01:56 +0100

    Restore "Updated macros for final analysis""
    This reverts commit 8096af3f35f43923346a2f953b19b4f9cdcea28c.

--- phiXeXe/runFinalAnalysis.C
+++ phiXeXe/runFinalAnalysis.C
@@ -1,12 +1,13 @@
 //#include "/Users/fbellini/alice/macros/ResonAnT/phiXeXe/projectPhiXeXe.C"
 #include "/Users/fbellini/alice/macros/ResonAnT/phiXeXe/FitSpectrum.C"
-void runFinalAnalysis()
+void runFinalAnalysis(bool dobgbwonly=0, bool dosys=1)
 {
   //projectPhiXeXe("20200615_RsnOut_B3.root", "phi", "RsnOut_tpc2sPtDep_tof3sveto5smism", "B3", 0, 0);
-  FitSpectrum(-1, "bgbw", 0.5, 10.0, "30oct20");
-  FitSpectrum(-1, "boltz", 0.5, 5.0, "30oct20");
-  FitSpectrum(-1, "bose", 0.5, 5.0, "30oct20");
-  FitSpectrum(-1, "levy", 0.5, 10.0, "30oct20");
-  FitSpectrum(-1, "mtexp", 0.5, 5.0, "30oct20");
+  if (dobgbwonly) FitSpectrum(-1, "bgbw", 0.5, 10.0, "04dec20");
+  if (!dosys) return;
+  FitSpectrum(-1, "boltz", 0.5, 5.0, "04dec20");
+  FitSpectrum(-1, "bose", 0.5, 5.0, "04dec20");
+  FitSpectrum(-1, "levy", 0.5, 10.0, "04dec20");
+  FitSpectrum(-1, "mtexp", 0.5, 5.0, "04dec20");
   return;
 }
